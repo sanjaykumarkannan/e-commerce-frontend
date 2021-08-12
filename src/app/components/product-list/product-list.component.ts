146f0bef031240c70d8f51f004db659cc9d94848
sanjaykumarkannan <sanjaykumarkannan@gmail.com>
2021-08-13 01:54:01 +0530

    Committing code with navigation between two working categories

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,26 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../../services/product.service';
 import { Product } from '../../common/product';
+import { ActivatedRoute } from '../../../../node_modules/@angular/router';
 
 @Component({
   selector: 'app-product-list',
   //templateUrl: './product-list.component.html',
-  templateUrl: './product-list-table.component.html',
+  //templateUrl: './product-list-table.component.html',
+  templateUrl: './product-list-grid.component.html',
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
-  products:Product[];
-  
-  constructor(private productService:ProductService) { }
+  products: Product[];
+  currentCategoryId: number;
+
+  //Activated route helps in finding the current active route of the component
+  constructor(private productService: ProductService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.listProducts();
+    this.route.paramMap.subscribe(() => {
+      this.listProducts();
+    }
+    );
   }
 
-  listProducts(){
-    this.productService.getProductList().subscribe(
-      data=>{
-        this.products=data;
+  listProducts() {
+
+    //check if id parameter is available
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+
+    if(hasCategoryId){
+      //get the "id" param string. convert string to a number using the "+" symbol
+      this.currentCategoryId=+this.route.snapshot.paramMap.get('id');
+    }
+    else{
+      //not category available...default category is 1...
+      this.currentCategoryId=1;
+    }
+
+    this.productService.getProductList(this.currentCategoryId).subscribe(
+      data => {
+        this.products = data;
       }
     )
   }
